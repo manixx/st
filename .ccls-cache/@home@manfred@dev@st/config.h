4bdb4528bcdb6ddfd9db28813fc995be86ecda49
Manfred Krieger <manixx90@gmail.com>
2025-09-27 20:01:29 +0200

    chore: enable paste

--- .ccls-cache/@home@manfred@dev@st/config.h
+++ .ccls-cache/@home@manfred@dev@st/config.h
@@ -106,7 +106,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.85;
+float alpha = 0.8; /*0.87;*/
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
@@ -203,7 +203,7 @@ static Shortcut shortcuts[] = {
 	//{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	//{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
 	//{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
-	//{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
+	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	//{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 };
 
